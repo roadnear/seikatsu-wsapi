6e1bfd06a21107d3842582a2d2acf896cff23e38
Rodner <rodnerraymundo@yahoo.com>
2017-11-15 00:43:28 +0400

    injected seed routes

--- app.js
+++ app.js
@@ -4,26 +4,27 @@ const mongoose = require('mongoose');
 const router = express.Router();
 const constants = require('./constants')();
 const Promise = require('bluebird');
+global.Promise = Promise;
 mongoose.Promise = Promise;
 const promise = mongoose.connect(constants.DATABASE_NAME, {
 	useMongoClient: true
 });
-const db = require('./data-access-layer/main')(mongoose);
+const db = require('./data-access-layer/index')(mongoose);
 
-console.log(global.Promise === require('bluebird'))
 promise.then(db => {
 	console.log('Database ' + db.name + ' connected successfuly on port ' + db.port);
 }, err => {
 	console.log('Database failed to connect: ', err);
 });
 
-const services = require('./services/main')(constants, db);
-require('./controllers/project')(constants, router, services);
+const models = require('./models/index')(constants, db);
+require('./routes/project')(constants, router, models);
+require('./routes/seed')(constants, router, models);
 
 app.use('/api/v1', router);
 app.use((err, req, res, next) => {
-	console.error(err);
-	res.status(500).send();
+	console.error('middleware == ', err);
+	return res.status(500).send();
 });
 
 app.listen(3232, () => console.log('App is listening on port:', 3232));
