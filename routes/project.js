ec75e6e1d670ffb2b6da02ba2637ff9f2eb10a16
Rodner <rodnerraymundo@yahoo.com>
2017-11-16 20:10:33 +0400

    Added post method

--- routes/project.js
+++ routes/project.js
@@ -1,16 +1,15 @@
 module.exports = function (constants, router, models) {
 
 	const errorHandler = require('../utils/error-handler');
-	const ProjectQuery = require('../models/project.query');
+	const ProjectTypes = require('../models/project.types');
 	const prefixUri = '/' + constants.PROJECTS;
 	const projectModel = models.project;
 
 	router.get(prefixUri, (req, res) => {
 		console.log('GET: ' + req.originalUrl + ' -- req.query == ', req.query);
 
-		const query = new ProjectQuery(req.query);
+		const query = new ProjectTypes.query(req.query);
 		console.log('query == ', query)
-
 		projectModel.find(query, { select: null}).then((projects) => {
 			if (!projects.length) {
 				return res.status(404).send();
@@ -23,5 +22,18 @@ module.exports = function (constants, router, models) {
 		});
 
 	});
-	
+
+	router.post(prefixUri, (req, res) => {
+		console.log('POST: ' + req.originalUrl + ' -- req.body == ', req.body);
+
+		const project = new ProjectTypes.create(req.body);
+
+		projectModel.create(project).then((createdProject) => {
+			return res.status(201).json(createdProject);
+		}).catch((error) => {
+			errorHandler(error, res);
+		});
+
+	});
+
 }
