4a7336b069e667f9dc8d307b50ce63199bfaaef7
Rodner <rodnerraymundo@yahoo.com>
2017-11-18 20:17:19 +0400

    Error handler method now checks if code is a valid http code

--- utils/error-handler.js
+++ utils/error-handler.js
@@ -1,9 +1,15 @@
 module.exports = function (error, resolve) {
 	if (!error || !resolve) return;
-	resolve.status(error.code || 500);
+	error.code = isValidHttpCode(error.code) ? error.code : 500;
+	resolve.status(error.code);
 	if (error.code === 500) {
 		console.error(error);
 		return resolve.send();
 	}
 	return resolve.json(error);
+}
+
+function isValidHttpCode(code) {
+	const codeRegEx = /^[1-5][0-9][0-9]$/
+	return codeRegEx.test(code);
 }
