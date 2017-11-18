0f39ac461eeeec135e823b04169daef5e11785d0
Rodner <rodnerraymundo@yahoo.com>
2017-11-18 20:14:31 +0400

    Added object Options

--- models/project.types.js
+++ models/project.types.js
@@ -1,7 +1,7 @@
 module.exports = {
 	query: function (query) {
 		this.is_deleted = query.is_deleted || false;
-		if (query.hasOwnProperty('title')) this.title = query.title;
+		if (query.hasOwnProperty('title')) this.title = { $regex: new RegExp(query.title, 'i') };
 		if (query.created_at_from) {
 			this.created_at = {
 				$gte: query.created_at_from
@@ -18,5 +18,20 @@ module.exports = {
 		this.title = project.title;
 		this.description = project.description;
 		this.skills = project.skills;
+	},
+	options: function (options, userType) {
+		if (typeof options !== 'object') throw new Error(`${options} invalid type`);
+		const page = options.page || 1;
+		this.limit = 10;
+		this.skip = this.limit * (page - 1);
+		this.select = {
+			__v: 0,
+			updated_at: 0,
+			is_deleted: 0
+		};
+
+		if (typeof userType === 'string') {
+			//object modification, to be added
+		}
 	}
 }
